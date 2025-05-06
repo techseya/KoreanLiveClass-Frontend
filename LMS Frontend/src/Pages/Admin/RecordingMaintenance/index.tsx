ae5e520a4a29e57b85982ae8c5d64ae495f14ee2
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2025-05-07 00:36:22 +0530

    commit

---
+++ LMS Frontend/src/Pages/Admin/RecordingMaintenance/index.tsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import CommanLayout from 'src/Layout/CommanLayout';
+import CategoryMaintenance from './SectionMaintenance';
+import CategoryForm from './SectionForm';
+import CourseMaintenance from './SectionMaintenance';
+import CourseForm from './SectionForm';
+import SectionMaintenance from './SectionMaintenance';
+import SectionForm from './SectionForm';
+
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+        </div>
+    );
+}
+
+function a11yProps(index: number) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
+export default function Recordings() {
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
+
+    return (
+        <CommanLayout name="Section Maintenance" path="section-maintenance">
+            <Box sx={{ width: '100%' }}>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                    <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                        <Tab label="Sections" {...a11yProps(0)} />
+                        <Tab label="Add Section" {...a11yProps(1)} />
+                    </Tabs>
+                </Box>
+                <CustomTabPanel value={value} index={0}>
+                    <SectionMaintenance />
+                </CustomTabPanel>
+                <CustomTabPanel value={value} index={1}>
+                    <SectionForm />
+                </CustomTabPanel>
+            </Box>
+        </CommanLayout>
+    );
+}
