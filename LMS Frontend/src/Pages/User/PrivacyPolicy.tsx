bd1aa3211360e84e7470bd96f1dae210bd0de5cd
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2025-05-18 19:36:18 +0530

    commit

---
+++ LMS Frontend/src/Pages/User/PrivacyPolicy.tsx
@@ -0,0 +1,10 @@
+import "../../Common/styles/privacy.css"
+import "../../Common/styles/courses.css"
+import { useTranslation } from "react-i18next";
+
+export default function PrivacyPolicy(){
+    const { t } = useTranslation();
+    return(
+        <></>
+    )
+}
