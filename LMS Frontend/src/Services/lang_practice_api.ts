03d8a373bb9d76336166859a0cc28203410e2418
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2025-07-17 14:34:00 +0530

    Add CRUD operations for Language Practice Questions in lang_practice_api

--- LMS Frontend/src/Services/lang_practice_api.ts
+++ LMS Frontend/src/Services/lang_practice_api.ts
@@ -16,11 +16,37 @@ export const createLanguagePractice = async (body: any,token:any) => {
     }
 }
 
+export const createLanguagePracticeQuestion = async (body: any, token: any) => {
+    try {
+        const response = await axios.post(api + `language-practice/question`, body, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+        });
+        return response;
+    } catch (error) {
+        throw error;
+    }
+}
+
 export const updateLanguagePractice = async (body: any, token: any) => {
     try {
         const response = await axios.put(api + `language-practice`, body, {
             headers: {
                 Authorization: `Bearer ${token}`,
+            },
+        });
+        return response;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const updateLanguagePracticeQuestion = async (body: any, token: any) => {
+    try {
+        const response = await axios.put(api + `language-practice/question`, body, {
+            headers: {
+                "Content-Type": "multipart/form-data",
             },
         });
         return response;
@@ -42,9 +68,35 @@ export const deleteLanguagePractice = async (id: any, token: any) => {
     }
 }
 
+export const deleteLanguagePracticeQuestion = async (id: any, token: any) => {
+    try {
+        const response = await axios.delete(api + `language-practice/question/${id}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        return response;
+    } catch (error) {
+        throw error;
+    }
+}
+
 export const getLanguagePractices = async (token: any) => {
     try {
         const response = await axios.get(api + `language-practice`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        return response;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const getLanguagePracticeQuestions = async (practiceId: any, token: any) => {
+    try {
+        const response = await axios.get(api + `language-practice/question/${practiceId}`, {
             headers: {
                 Authorization: `Bearer ${token}`,
             },
